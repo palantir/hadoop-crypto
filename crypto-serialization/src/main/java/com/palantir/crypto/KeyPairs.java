51065a2fe737ca7743028e48f35eab0351c34abd
Joe Ellis <jellis@palantir.com>
2016-12-13 20:26:49 -0800

    move KeyPairs

--- crypto-serialization/src/main/java/com/palantir/crypto/KeyPairs.java
+++ crypto-serialization/src/main/java/com/palantir/crypto/KeyPairs.java
@@ -14,10 +14,11 @@
  * limitations under the License.
  */
 
-package com.palantir.hadoop;
+package com.palantir.crypto;
 
 import com.google.common.base.Optional;
 import com.google.common.base.Throwables;
+import com.google.common.io.BaseEncoding;
 import java.security.GeneralSecurityException;
 import java.security.KeyFactory;
 import java.security.KeyPair;
@@ -25,8 +26,6 @@ import java.security.PrivateKey;
 import java.security.spec.KeySpec;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
-import javax.annotation.Nullable;
-import org.apache.commons.net.util.Base64;
 
 public final class KeyPairs {
 
@@ -38,17 +37,17 @@ public final class KeyPairs {
      * @param privateKeyString base64 encoded PKCS8 private key
      * @param publicKeyString base64 encoded X509 public key
      */
-    public static KeyPair fromStrings(@Nullable String privateKeyString, String publicKeyString, String algorithm) {
+    public static KeyPair fromStrings(String privateKeyString, String publicKeyString, String algorithm) {
         try {
             KeyFactory keyFactory = KeyFactory.getInstance(algorithm);
             // Private key is only required for decryption, can be null
             Optional<PrivateKey> privateKey = Optional.absent();
             if (privateKeyString != null) {
-                KeySpec privateKs = new PKCS8EncodedKeySpec(Base64.decodeBase64(privateKeyString));
+                KeySpec privateKs = new PKCS8EncodedKeySpec(BaseEncoding.base64().decode(privateKeyString));
                 privateKey = Optional.of(keyFactory.generatePrivate(privateKs));
             }
 
-            KeySpec publicKs = new X509EncodedKeySpec(Base64.decodeBase64(publicKeyString));
+            KeySpec publicKs = new X509EncodedKeySpec(BaseEncoding.base64().decode(publicKeyString));
 
             return new KeyPair(keyFactory.generatePublic(publicKs), privateKey.orNull());
         } catch (GeneralSecurityException e) {
