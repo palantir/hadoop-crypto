3fa7f8027650c461d66be0ffc9d644342d6685c5
Joe Ellis <jellis@palantir.com>
2017-04-06 10:46:47 -0700

    add KeyPairsTest

---
+++ crypto-keys/src/test/java/com/palantir/crypto2/keys/KeyPairsTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.crypto2.keys;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
+import org.junit.Before;
+import org.junit.Test;
+
+public final class KeyPairsTest {
+
+    private KeyPair keyPair;
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+
+    @Before
+    public void before() {
+        keyPair = TestKeyPairs.generateKeyPair();
+        privateKey = keyPair.getPrivate();
+        publicKey = keyPair.getPublic();
+    }
+
+    @Test
+    public void testDeserialization() {
+        String privateEncoded = encode(privateKey.getEncoded());
+        String publicEncoded = encode(publicKey.getEncoded());
+        KeyPair deserialized = KeyPairs.fromStrings(privateEncoded, publicEncoded, "RSA");
+
+        // KeyPair does not implement equals so check each key individually
+        assertThat(deserialized.getPrivate(), is(privateKey));
+        assertThat(deserialized.getPublic(), is(publicKey));
+    }
+
+    private static String encode(byte[] bytes) {
+        return Base64.getEncoder().encodeToString(bytes);
+    }
+
+}
