e2e3441da2d7dc9f5be94512d4d51d27f95af699
Joe Ellis <jellis@palantir.com>
2017-08-15 14:25:49 +0100

    try again

--- verify-ibmjvm.sh
+++ verify-ibmjvm.sh
@@ -6,5 +6,5 @@ docker run --rm \
     -v "$HOME/.docker:/root/.docker" \
     -v "$HOME/.m2:/root/.m2" \
     -e OVERRIDE_KEY_SAFETY_PROTECTIONS=true \
-    'ibmjava:8-sdk@sha256:5334bbeaebe15e044f379879e1bb900c0a98f6f3e93fd7dbc6ea18bb9ae263ca' \
+    'ibmjava@sha256:5334bbeaebe15e044f379879e1bb900c0a98f6f3e93fd7dbc6ea18bb9ae263ca' \
     ./gradlew crypto-core:test
