388c416dff2aa4aa90ce1e06788cb898945ed6fb
Joe Ellis <jellis@palantir.com>
2017-08-15 14:47:55 +0100

    try to pull directly

--- verify-ibmjvm.sh
+++ verify-ibmjvm.sh
@@ -1,4 +1,5 @@
 # Runs the crypto-core tests on an ibm jvm
+docker pull ibmjava@sha256:5334bbeaebe15e044f379879e1bb900c0a98f6f3e93fd7dbc6ea18bb9ae263ca
 docker run --rm \
     -w '/workspace' \
     -v "$(pwd):/workspace" \
