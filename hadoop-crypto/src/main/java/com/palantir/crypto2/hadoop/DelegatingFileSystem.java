75bdf16f18e7313df3d37e0b5957a56fbdb747a5
Joe Ellis <jellis@palantir.com>
2017-09-25 19:01:00 +0100

    use delegating filesystem

---
+++ hadoop-crypto/src/main/java/com/palantir/crypto2/hadoop/DelegatingFileSystem.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.crypto2.hadoop;
+
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileUtil;
+import org.apache.hadoop.fs.FilterFileSystem;
+import org.apache.hadoop.fs.Path;
+
+/**
+ * Equivalent to {@link FilterFileSystem} but invokes {@link #create} and {@link #open} on this class when calling
+ * {@link #copyFromLocalFile} and {@link #copyToLocalFile}.
+ * <p>
+ * Solves: https://issues.apache.org/jira/browse/HADOOP-13870
+ */
+public abstract class DelegatingFileSystem extends FilterFileSystem {
+
+    public DelegatingFileSystem(FileSystem delegate) {
+        super(delegate);
+        super.setConf(delegate.getConf());
+    }
+
+    public void copyFromLocalFile(Path src, Path dst) throws IOException {
+        copyFromLocalFile(false, src, dst);
+    }
+
+    @Override
+    public void copyFromLocalFile(boolean delSrc, Path src, Path dst)
+            throws IOException {
+        copyFromLocalFile(delSrc, true, src, dst);
+    }
+
+    @Override
+    public void copyFromLocalFile(boolean delSrc, boolean overwrite, Path[] srcs, Path dst) throws IOException {
+        Configuration conf = getConf();
+        FileUtil.copy(getLocal(conf), srcs, this, dst, delSrc, overwrite, conf);
+    }
+
+    @Override
+    public void copyFromLocalFile(boolean delSrc, boolean overwrite, Path src, Path dst) throws IOException {
+        Configuration conf = getConf();
+        FileUtil.copy(getLocal(conf), src, this, dst, delSrc, overwrite, conf);
+    }
+
+    @Override
+    public void copyToLocalFile(Path src, Path dst) throws IOException {
+        copyToLocalFile(false, src, dst);
+    }
+
+    @Override
+    public void copyToLocalFile(boolean delSrc, Path src, Path dst) throws IOException {
+        copyToLocalFile(delSrc, src, dst, false);
+    }
+
+    @Override
+    public void copyToLocalFile(boolean delSrc, Path src, Path dst,
+            boolean useRawLocalFileSystem) throws IOException {
+        Configuration conf = getConf();
+        FileSystem local = useRawLocalFileSystem
+                ? getLocal(conf).getRaw()
+                : getLocal(conf);
+        FileUtil.copy(this, src, local, dst, delSrc, conf);
+    }
+}
