76c4440abeb63d70ab9d0b97056a81245a4ceb7e
Joe Ellis <jellis@palantir.com>
2018-02-27 19:52:23 +0000

    fix

--- hadoop-crypto/src/main/java/com/palantir/crypto2/hadoop/StandaloneEncryptedFileSystem.java
+++ hadoop-crypto/src/main/java/com/palantir/crypto2/hadoop/StandaloneEncryptedFileSystem.java
@@ -137,7 +137,7 @@ public final class StandaloneEncryptedFileSystem extends FilterFileSystem {
     public boolean delete(Path path, boolean recursive) throws IOException {
         // Since StandaloneEncryptedFileSystem uses a FileKeyStorageStrategy, the delegate delete call on folders
         // deletes both the payload files and the adjacent encryption materials. For files we can continue to allow
-        // on the EncryptedFileSystem handling removal of both the file and the key material.
+        // rely on the EncryptedFileSystem handling removal of both the file and the key material.
         if (fs.isFile(path)) {
             return fs.delete(path, false);
         } else {
