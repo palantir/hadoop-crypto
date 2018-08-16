853b8feed08de89bdf739823b5c8b4f71322a097
Joe Ellis <jellis@palantir.com>
2018-08-16 17:18:54 +0100

    log exceptions

--- crypto-core/src/main/java/com/palantir/crypto2/io/CryptoStreamFactory.java
+++ crypto-core/src/main/java/com/palantir/crypto2/io/CryptoStreamFactory.java
@@ -59,7 +59,7 @@ public final class CryptoStreamFactory {
         try {
             return new ApacheCtrDecryptingSeekableInput(encryptedInput, keyMaterial);
         } catch (IOException e) {
-            log.warn("Unable to initialize cipher with OpenSSL falling back to JCE implementation");
+            log.warn("Unable to initialize cipher with OpenSSL falling back to JCE implementation", e);
             return new DecryptingSeekableInput(encryptedInput, SeekableCipherFactory.getCipher(algorithm, keyMaterial));
         }
     }
@@ -89,7 +89,7 @@ public final class CryptoStreamFactory {
         try {
             return createApacheEncryptedStream(output, keyMaterial);
         } catch (IOException e) {
-            log.warn("Unable to initialize cipher with OpenSSL, falling back to JCE implementation");
+            log.warn("Unable to initialize cipher with OpenSSL, falling back to JCE implementation", e);
             return createDefaultEncryptedStream(output, keyMaterial, algorithm);
         }
     }
