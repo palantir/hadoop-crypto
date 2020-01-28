c0282da5624c1b09cb0b7d82e4fe3bd941c26c03
Dan Fox <dfox@palantir.com>
2020-01-28 16:32:39 +0000

    log-exception when unable to use fast codepath

--- crypto-core/src/main/java/com/palantir/crypto2/io/CryptoStreamFactory.java
+++ crypto-core/src/main/java/com/palantir/crypto2/io/CryptoStreamFactory.java
@@ -60,7 +60,7 @@ public final class CryptoStreamFactory {
         try {
             return new ApacheCtrDecryptingSeekableInput(encryptedInput, keyMaterial);
         } catch (IOException e) {
-            log.warn("Unable to initialize cipher with OpenSSL falling back to JCE implementation");
+            log.warn("Unable to initialize cipher with OpenSSL falling back to JCE implementation", e);
             return new DecryptingSeekableInput(encryptedInput, SeekableCipherFactory.getCipher(algorithm, keyMaterial));
         }
     }
@@ -91,7 +91,7 @@ public final class CryptoStreamFactory {
         try {
             return createApacheEncryptedStream(output, keyMaterial);
         } catch (IOException e) {
-            log.warn("Unable to initialize cipher with OpenSSL, falling back to JCE implementation");
+            log.warn("Unable to initialize cipher with OpenSSL, falling back to JCE implementation", e);
             return createDefaultEncryptedStream(output, keyMaterial, algorithm);
         }
     }
