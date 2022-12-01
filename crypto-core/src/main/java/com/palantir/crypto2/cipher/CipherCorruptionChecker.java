46584ec5c7996214263c168a715ab7799bcd056e
James Baker <jbaker@palantir.com>
2022-12-01 11:26:18 +0000

    parallel

--- crypto-core/src/main/java/com/palantir/crypto2/cipher/CipherCorruptionChecker.java
+++ crypto-core/src/main/java/com/palantir/crypto2/cipher/CipherCorruptionChecker.java
@@ -1,17 +1,23 @@
 package com.palantir.crypto2.cipher;
 
+import com.google.common.util.concurrent.Futures;
 import java.security.GeneralSecurityException;
 import java.security.Key;
 import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
 import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public final class CipherCorruptionChecker {
-    private static final int LOOPS = 100000000;
+    private static final int LOOPS = 1_000_000;
     private static final int LEN = 15;
 
     private CipherCorruptionChecker() {}
@@ -20,10 +26,12 @@ public final class CipherCorruptionChecker {
         for (int i = 0; i < 100_000; i++) {
             isCorruptionPresent(1);
         }
-        return IntStream.range(0, 64)
-                .parallel()
-                .mapToObj(_index -> isCorruptionPresent(LOOPS))
-                .reduce(false, Boolean::logicalOr);
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        List<Future<Boolean>> futures = IntStream.range(0, 64)
+                .mapToObj(_index -> executorService.submit(() -> isCorruptionPresent(LOOPS)))
+                .collect(Collectors.toUnmodifiableList());
+        executorService.shutdown();
+        return futures.stream().map(Futures::getUnchecked).reduce(false, Boolean::logicalOr);
     }
 
     private static boolean isCorruptionPresent(int loops) {
