649b3e501a3f71b3f571f6d07444bd405b2034b8
David Schlosnagle <davids@palantir.com>
2026-09-16 16:07:02 +0000

    Only test Apache streams when OpenSSL is available

--- crypto-core/src/test/java/com/palantir/crypto2/io/CryptoStreamFactoryTest.java
+++ crypto-core/src/test/java/com/palantir/crypto2/io/CryptoStreamFactoryTest.java
@@ -17,6 +17,7 @@
 package com.palantir.crypto2.io;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import com.google.common.io.ByteStreams;
 import com.palantir.crypto2.cipher.AesCtrCipher;
@@ -52,9 +53,7 @@ public final class CryptoStreamFactoryTest {
     @Test
     @EnabledOnOs(OS.LINUX)
     public void ensureDefaultIsApache() throws IOException {
-        assertThat(CryptoStreamFactory.isOpenSslAvailable())
-                .as("OpenSSL available")
-                .isTrue();
+        assumeTrue(CryptoStreamFactory.isOpenSslAvailable());
         try (OutputStream encrypted =
                         CryptoStreamFactory.encrypt(new ByteArrayOutputStream(), keyMaterial, AesCtrCipher.ALGORITHM);
                 SeekableInput decrypted = CryptoStreamFactory.decrypt(
