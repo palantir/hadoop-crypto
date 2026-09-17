3678e81d9445a85b1b9bbef8ab0a1f9b1c07d12a
Joe Ellis <jellis@palantir.com>
2026-09-16 18:26:32 -0600

    Remove Apache Commons Crypto
    Java has added native support for intrinics and as such we no longer
    need to delegate to OpenSSL via commons-crypto.

--- crypto-core/src/test/java/com/palantir/crypto2/io/CryptoStreamFactoryTest.java
+++ crypto-core/src/test/java/com/palantir/crypto2/io/CryptoStreamFactoryTest.java
@@ -21,8 +21,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.google.common.io.ByteStreams;
 import com.palantir.crypto2.cipher.AesCtrCipher;
 import com.palantir.crypto2.keys.KeyMaterial;
-import com.palantir.seekio.InMemorySeekableDataInput;
-import com.palantir.seekio.SeekableInput;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -30,16 +28,11 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ThreadLocalRandom;
-import org.apache.commons.crypto.stream.CtrCryptoInputStream;
-import org.apache.commons.crypto.stream.CtrCryptoOutputStream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledOnOs;
-import org.junit.jupiter.api.condition.OS;
 
 public final class CryptoStreamFactoryTest {
 
-    private static final boolean FORCE_JCE = true;
     private static final byte[] BYTES = "data".getBytes(StandardCharsets.UTF_8);
 
     private KeyMaterial keyMaterial;
@@ -47,17 +40,6 @@ public final class CryptoStreamFactoryTest {
     @BeforeEach
     public void before() {
         keyMaterial = AesCtrCipher.generateKeyMaterial();
-    }
-
-    @Test
-    @EnabledOnOs(OS.LINUX)
-    public void ensureDefaultIsApache() {
-        OutputStream encrypted = CryptoStreamFactory.encrypt(null, keyMaterial, AesCtrCipher.ALGORITHM);
-        SeekableInput decrypted =
-                CryptoStreamFactory.decrypt((SeekableInput) null, keyMaterial, AesCtrCipher.ALGORITHM);
-
-        assertThat(encrypted).isInstanceOf(CtrCryptoOutputStream.class);
-        assertThat(decrypted).isInstanceOf(CtrCryptoInputStream.class);
     }
 
     @Test
@@ -71,23 +53,6 @@ public final class CryptoStreamFactoryTest {
                 new ByteArrayInputStream(os.toByteArray()), keyMaterial, AesCtrCipher.ALGORITHM);
 
         assertThat(ByteStreams.toByteArray(decrypted)).isEqualTo(BYTES);
-    }
-
-    @Test
-    public void testEncryptDecryptJce() throws IOException {
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        OutputStream encrypted = CryptoStreamFactory.encrypt(os, keyMaterial, AesCtrCipher.ALGORITHM, FORCE_JCE);
-        encrypted.write(BYTES);
-        encrypted.close();
-
-        SeekableInput decrypted = CryptoStreamFactory.decrypt(
-                new InMemorySeekableDataInput(os.toByteArray()), keyMaterial, AesCtrCipher.ALGORITHM, FORCE_JCE);
-
-        byte[] readBytes = new byte[BYTES.length];
-        int bytesRead = decrypted.read(readBytes, 0, BYTES.length);
-
-        assertThat(bytesRead).isEqualTo(BYTES.length);
-        assertThat(readBytes).isEqualTo(BYTES);
     }
 
     @Test
